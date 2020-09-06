c3b724d03bb024d84b552351fbbd79b4b649dc4a
Kaj Białas <kaj.bialas@dev-industry.com>
2020-09-06 15:28:22 +0200

    Hooks redux for counter

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { COUNTER_ACTION_TYPES} from '../modules/counter/counter.action';
 import TodoList from '../components/TodoList';
+import { useSelector, useDispatch } from 'react-redux';
 
-function App({count, incrementCounter, decrementCounter, resetCounter, changeCounter}) {
+function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
       Counter: {count}
-      <button onClick={incrementCounter} >+</button>
-      <button onClick={decrementCounter} >-</button>
-      <button onClick={resetCounter} >RESET</button>
-      <input value={count} onChange={changeCounter} />
+      <button onClick={() => dispatch({type: COUNTER_ACTION_TYPES.INCREMENT})} >+</button>
+      <button onClick={() => dispatch({type: COUNTER_ACTION_TYPES.DECREMENT})} >-</button>
+      <button onClick={() => dispatch({type: COUNTER_ACTION_TYPES.RESET})} >RESET</button>
+      <input value={count} onChange={(e) => dispatch({type: COUNTER_ACTION_TYPES.CHANGE, payload: e.target.value})} />
       <TodoList />
     </div>
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    count: state.counter.value,
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    incrementCounter: () => dispatch({type: COUNTER_ACTION_TYPES.INCREMENT}),
-    decrementCounter: () => dispatch({type: COUNTER_ACTION_TYPES.DECREMENT}),
-    resetCounter: () => dispatch({type: COUNTER_ACTION_TYPES.RESET}),
-    changeCounter: (e) => dispatch({type: COUNTER_ACTION_TYPES.CHANGE, payload: e.target.value})
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
